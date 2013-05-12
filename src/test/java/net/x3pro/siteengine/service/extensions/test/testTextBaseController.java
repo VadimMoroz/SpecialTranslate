f0d8fd1b60d7146b62eb81ff2dab10438835d28c
VadimMoroz <i.vadim.moroz@gmail.com>
2013-05-12 22:33:17 +0300

    test modified

--- src/test/java/net/x3pro/siteengine/service/extensions/test/testTextBaseController.java
+++ src/test/java/net/x3pro/siteengine/service/extensions/test/testTextBaseController.java
@@ -2,10 +2,9 @@ package net.x3pro.siteengine.service.extensions.test;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "classpath:spring-config.xml" })
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(locations = { "classpath:spring-config.xml" })
 public class testTextBaseController {
 	
 	@Test
